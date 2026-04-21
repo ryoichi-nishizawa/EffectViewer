6d5d89e19974d06051fbdec08730bd057c5fadb7
ryoichi-nishizawa <nishizawa.ryoichi.0309@gmail.com>
2026-04-22 02:36:18 +0900

    Create Scene Manager

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    // --------------------------------------------------
+    // SceneManager Class
+    // --------------------------------------------------
+
+    public class SceneManager : MonoBehaviour
+    {
+        void Awake()
+        {
+        }
+
+        void Start()
+        {
+        }
+
+        void Update()
+        {
+        }
+    }
+}
